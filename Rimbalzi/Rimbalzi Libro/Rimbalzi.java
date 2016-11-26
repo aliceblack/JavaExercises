aeb7f0349238de5211be75ef490a168f6408854d
Alice Black <blackfluid44@gmail.com>
2016-11-26 15:31:26 +0100

    Esercizio Rimbalzi.java libro

---
+++ Rimbalzi/Rimbalzi Libro/Rimbalzi.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Rimbalzi{
+	public static void main(String[] a){
+		JFrame frame=new RimbalziFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+
+
+
+
+}
+
